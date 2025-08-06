e2cf35ae6c4e8a0284cf6f54da7b0ab4fb86e543
BoMeyering <bomeyering@proton.me>
2025-08-06 12:13:54 -0400

    Final segmentation model code

--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 """
+src/models.py
 Models Stub script
 BoMeyering 2025
 """
@@ -8,26 +9,6 @@ import inspect
 import segmentation_models_pytorch as smp
 from transformers import SegformerForSemanticSegmentation
 
-
-
-def create_model(num_classes: int, variant: str='nvidia/mit-b0'):
-    variants = [
-        'nvidia/mit-b0', 
-        'nvidia/mit-b1', 
-        'nvidia/mit-b2', 
-        'nvidia/mit-b3', 
-        'nvidia/mit-b4', 
-        'nvidia/mit-b5'
-    ]
-    if variant in variants:
-        model = SegformerForSemanticSegmentation.from_pretrained(
-            variant,
-            num_labels=num_classes,
-        )
-        return model
-    else:
-        raise ValueError(f"Arg 'variant' must be one of {variants}")
-    
 
 def create_smp_model(config: dict) -> torch.nn.Module:
     """Creates an smp Pytorch model
